f3c87cc9b4bdd4ca3015198fc89f2ad954966cc5
HugoVec <42803993+HugoVec@users.noreply.github.com>
2021-09-02 22:04:40 -0300

    algoritmo paginação v1
    primeira versão

--- paginacao.py
+++ paginacao.py
@@ -2,16 +2,33 @@ import random
 
 def paginacao():
     
-    memoria_fisica = [1]*10
-    tamanho = random.randint(2, 30)
+    memoria_fisica = [0]*10
+    espaco_usado = random.randint(2, 9)
+    memoria_disponivel = len(memoria_fisica) - espaco_usado
 
-    print(memoria_fisica)
-
-    # mudar numero 50 pra um random 
-    for i in range(30):
-        memoria_fisica[i] = memoria_fisica[i] + 1
+    # Aloca espaco usado aleatoriamente na memoria fisica
+    for i in range(espaco_usado):
+        memoria_fisica[i] = 1
         
     print(memoria_fisica)
+
+    # Pede ao usuário a memoria logia a ser inserida na memoria fisica
+    memoria_logica = int(input('Ola usuario, qual o tamanho de memoria logica que deseja inserir? '))
+
+    # Caso a memoria logica pedida seja maior que a memoria disponivel ele pede que insira uma nova
+    while memoria_logica > memoria_disponivel:
+        memoria_logica = int(input('Por favor insira uma quantidade menor ou igual a: '))
+
+    # Aloca a memoria logica na fisica
+    for x in range(len(memoria_fisica)):
+        if memoria_fisica[x] == 0 and memoria_logica > 0:
+            memoria_fisica[x] = 2
+            memoria_logica = memoria_logica - 1
+
+    print(memoria_fisica)
+
+    
+    
 
 
 
